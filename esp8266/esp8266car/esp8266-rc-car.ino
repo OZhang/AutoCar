4ec492d880c68370cff325471dcc234077267723
OZhang <zhangrunze@163.com>
2019-01-05 19:42:18 +0800

    updated esp8266

--- esp8266/esp8266car/esp8266-rc-car.ino
+++ esp8266/esp8266car/esp8266-rc-car.ino
@@ -1,8 +1,8 @@
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 
-const char* ssid = "ty200824G"; //Enter your wifi network SSID
-const char* password = "hongjie@1982"; //Enter your wifi network password
+const char* ssid = "***********************"; //Enter your wifi network SSID
+const char* password = "*******************"; //Enter your wifi network password
 
 //Motor B
 int PWMB = 16; //Speed control-d0
